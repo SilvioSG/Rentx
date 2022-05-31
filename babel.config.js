8ed816457955d7c1bfb7b518b1c8fb241931b1e4
Silvio Souza <98766439+SilvioSG@users.noreply.github.com>
2022-05-31 20:52:48 -0300

    Atualizando

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,9 @@ module.exports = {
           },
         },
       ],
-      "babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      'babel-plugin-transform-typescript-metadata',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ],
 };
